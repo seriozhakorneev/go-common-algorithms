3daabb96c6f77de3a5275259ab37bb1b2761cc15
korneevsergey <seriozhakorneev@gmail.com>
2022-08-19 19:36:35 +0500

    tree
    Add depth-first traversals to tree

---
+++ tree/depthfirst/depthfirst_test.go
@@ -0,0 +1,49 @@
+package depthfirst
+
+import (
+	"github.com/seriozhakorneev/go-data-structures/trees/binarytree"
+	"reflect"
+	"testing"
+)
+
+func testTree() *binarytree.Node[string] {
+	t := binarytree.NewTree("F")
+
+	t.Root.Left = &binarytree.Node[string]{Data: "B"}
+	t.Root.Left.Left = &binarytree.Node[string]{Data: "A"}
+	t.Root.Left.Right = &binarytree.Node[string]{Data: "D"}
+	t.Root.Left.Right.Left = &binarytree.Node[string]{Data: "C"}
+	t.Root.Left.Right.Right = &binarytree.Node[string]{Data: "E"}
+
+	t.Root.Right = &binarytree.Node[string]{Data: "G"}
+	t.Root.Right.Right = &binarytree.Node[string]{Data: "I"}
+	t.Root.Right.Right.Left = &binarytree.Node[string]{Data: "H"}
+
+	return t.Root
+}
+
+func TestPreOrder(t *testing.T) {
+	expected := []string{"F", "B", "A", "D", "C", "E", "G", "I", "H"}
+	stack := PreOrder[string](testTree())
+
+	if !reflect.DeepEqual(expected, stack) {
+		t.Fatalf("Expected stack: %v\n Got: %v", expected, stack)
+	}
+
+}
+func TestInOrder(t *testing.T) {
+	expected := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
+	stack := InOrder[string](testTree())
+
+	if !reflect.DeepEqual(expected, stack) {
+		t.Fatalf("Expected stack: %v\n Got: %v", expected, stack)
+	}
+}
+func TestPostOrder(t *testing.T) {
+	expected := []string{"A", "C", "E", "D", "B", "H", "I", "G", "F"}
+	stack := PostOrder[string](testTree())
+
+	if !reflect.DeepEqual(expected, stack) {
+		t.Fatalf("Expected stack: %v\n Got: %v", expected, stack)
+	}
+}
