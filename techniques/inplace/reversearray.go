e8a4bb2b594b14f8b0fb82cbbe2b5a650c21c00e
korneevsergey <seriozhakorneev@gmail.com>
2022-08-23 22:47:50 +0500

    tree
    Minor fix

--- techniques/inplace/reversearray.go
+++ techniques/inplace/reversearray.go
@@ -2,9 +2,8 @@ package inplace
 
 func reverseArray(array []int) {
 	length := len(array)
-	for i, j := 0, length-1; i < length/2; i++ {
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
 		array[i], array[j] = array[j], array[i]
-		j--
 	}
 
 	// or
